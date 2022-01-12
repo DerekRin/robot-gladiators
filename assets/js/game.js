10e727432cb9faf99ca7919755b2f930de6e779c
DerekRin <derekrin98@gmail.com>
2022-01-12 11:00:31 -0600

    Fixed skip bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,18 +12,10 @@ var randomNumber = function(min, max) {
 // fight();
 var fight = function (enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP? Enter 'FIGHT' or 'SKIP' to choose.");
-
-if (promptFight === "skip" || promptFight === "SKIP") {
-    var confirmSkip = window.confirm("Are you sure you'd like to QUIT?");
-
-    if (confirmSkip) {
-    window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-    playerInfo.money = Math.max(0, playerInfo.money - 10);
-    console.log("playerInfo.money", playerInfo.money);
-    break;
+    if (fightOrSkip()); {
+        break;
     }
-}
+
     // remove enemy's health by subtracking the amount set in the playerInfo.attack variable
 var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
@@ -61,6 +53,26 @@ if (playerInfo.health <= 0) {
 } 
 };
 
+var fightOrSkip = function() {
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    promptFight = promptFight.toLowerCase();
+    if (promptFight === "skip") {
+    var confirmSkip = window.confirm("Are you sure you'd like to QUIT?");
+
+    if (confirmSkip) {
+    window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+    playerInfo.playerMoney = playerInfo.money - 10;
+    return true;
+        }
+    }
+}
+
 //function to start a new game
 var startGame = function () { 
         //reset player stats
@@ -87,6 +99,7 @@ for(var i = 0; i < enemyInfo.length; i++) {
     //play again
     endGame();
 };
+
 
 
 var endGame = function () {
